ec061af9f29e43d2abdfdf265e5f66989a7bdab8
Sameer <sameer.882000@gmail.com>
2020-11-26 18:12:38 +0530

    boxes can be checked now.

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = true;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task'),
-      trailing: Checkbox(
-        value: false,
+      title: Text(
+        'This is a task',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
+      trailing: TaskCheckbox(
+          checkboxState: isChecked,
+          toggleCheckboxState: (bool checkboxState) {
+            setState(() {
+              isChecked = checkboxState;
+            });
+          }),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final Function toggleCheckboxState;
+
+  TaskCheckbox({this.checkboxState, this.toggleCheckboxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: toggleCheckboxState,
     );
   }
 }
